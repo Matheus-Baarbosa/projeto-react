fa738f47bf56d5b94d19ad515dc83cb163822721
Matheus Barbosa <tetofelixx@gmail.com>
2025-09-14 16:47:03 -0300

    Efeitos e Eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import ContadorSeguro from "./components/ContadorSeguro";
+import TextoDinamico from "./components/TextoDinamico";
 export default function App() {
   return (
     <main style={{ fontFamily: "sans-serif", padding: 16 }}>
-      <h1>Atualizações Seguras de Estado</h1>
-      <ContadorSeguro />
+      <TextoDinamico />
     </main>
   );
 }
